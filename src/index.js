27c208b75f2e2eeb9771d202090eccd884eb9f0f
JohnFuhrm12 <61069716+JohnFuhrm12@users.noreply.github.com>
2023-08-03 00:12:50 -0400

    Tab Loading (Delete Previous Then Create Again)

--- src/index.js
+++ src/index.js
@@ -1,6 +1,36 @@
-import {loadMain} from './mainPage.js';
+import { loadMain } from './mainPage.js';
+import { loadMenu } from './menuPage.js';
+
+const content = document.getElementById('content');
+
+// Nabvar Event Listeners
+function loadListeners() {
+    const homeTab = document.getElementById('home');
+    homeTab.addEventListener('click', function() {
+        content.innerHTML = '';
+        loadMain();
+        loadListeners();
+        console.log('loaded home');
+    });
+
+    const menuTab = document.getElementById('menu');
+    menuTab.addEventListener('click', function() {
+        content.innerHTML = '';
+        loadMenu();
+        loadListeners();
+        console.log('loaded menu');
+    });
+
+    const contactTab = document.getElementById('contact');
+    contactTab.addEventListener('click', function() {
+        content.innerHTML = '';
+        loadListeners();
+        console.log('loaded contact');
+    });
+};
 
 // Load Main Page HTML Onload
 window.onload = function() {
     loadMain();
+    loadListeners();
 };
--- src/menuPage.js
+++ src/menuPage.js
@@ -1,4 +1,4 @@
-const loadMain = () => {
+const loadMenu = () => {
     const content = document.getElementById('content');
 
     // Navbar Creation
@@ -27,23 +27,8 @@ const loadMain = () => {
     // Title Creation
     const title = document.createElement('h1');
     title.id = 'title';
-    title.innerText = 'FOGÃO DO RIO';
+    title.innerText = 'CARDÁPIO';
     content.appendChild(title);
-
-    // About Section Creation
-    const aboutSection = document.createElement('div');
-    aboutSection.id = 'aboutSection';
-    content.appendChild(aboutSection);
-
-    const subTitle = document.createElement('h2');
-    subTitle.className = 'subTitle';
-    subTitle.innerText = 'O FOGÃO';
-    aboutSection.appendChild(subTitle);
-
-    const paragraphBlock = document.createElement('p');
-    paragraphBlock.className = 'paragraphBlock';
-    paragraphBlock.innerText = 'Localizado na cidade de Rio de Janeiro, com a praia de Ipanema na frente, experimente o sabor de um churraso rodízio irresistível. Temos a paixão de colocar na mesa pratos de alta qualidade, são selecionados da melhor carne da região.';
-    aboutSection.appendChild(paragraphBlock);
 };
 
-export {loadMain};
+export {loadMenu};
